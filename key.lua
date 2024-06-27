8b1ad62226a735528a6b38809998762045c38ba6
liebertsx <drocbave@gmail.com>
2024-06-27 22:34:23 +0700

    Update key.lua

--- key.lua
+++ key.lua
@@ -1,13 +1,13 @@
--- Made By liebertsx#0000
--- Enjoy Skid Or remake (credit)
--- Im making this script,ui on mobile
+-- Created by Liebert
+-- You Can modify this , but use credit
+-- Im making this on mobile lol
 
 local lib = {}
 
 function lib:Create(key, link, script)
     local Key = Instance.new("ScreenGui")
     Key.Name = "Key"
-    Key.Parent = game:GetService("Players").LocalPlayer:FindFirstChild("PlayerGui")
+    Key.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
     Key.ResetOnSpawn = false
 
     local Frame = Instance.new("Frame", Key)
@@ -20,14 +20,14 @@ function lib:Create(key, link, script)
 
     local Title = Instance.new("TextLabel", Frame)
     Title.Name = "Title"
-    Title.Font = Enum.Font.BuilderSansExtraBold
+    Title.Font = Enum.Font.SourceSansBold
     Title.FontSize = Enum.FontSize.Size24
     Title.Text = "Key System"
     Title.TextColor3 = Color3.fromRGB(255, 255, 255)
     Title.BackgroundTransparency = 0.1
     Title.BorderSizePixel = 0
-    Title.Size = UDim2.new(0, 325, 0, 50)
-    Title.BackgroundColor3 = Color3.fromRGB(30,30,30)
+    Title.Size = UDim2.new(1, 0, 0, 50)
+    Title.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
     Instance.new("UICorner", Title)
 
     local exit = Instance.new("ImageButton", Title)
@@ -38,40 +38,40 @@ function lib:Create(key, link, script)
     exit.Size = UDim2.new(0, 20, 0, 20)
 
     local Frame_2 = Instance.new("Frame", Frame)
-    Frame_2.Name = "Frame"
+    Frame_2.Name = "Frame_2"
     Frame_2.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
     Frame_2.BorderSizePixel = 0
     Frame_2.Position = UDim2.new(0, 0, 0, 45)
-    Frame_2.Size = UDim2.new(0, 325, 0, 10)
+    Frame_2.Size = UDim2.new(1, 0, 0, 10)
 
     local Desc = Instance.new("TextLabel", Frame_2)
     Desc.Name = "Desc"
-    Desc.Font = Enum.Font.BuilderSansExtraBold
+    Desc.Font = Enum.Font.SourceSansBold
     Desc.FontSize = Enum.FontSize.Size18
-    Desc.Text = "Hi! Welcome to Key System"
+    Desc.Text = "Hi! Welcome to Key System👋"
     Desc.TextColor3 = Color3.fromRGB(255, 255, 255)
     Desc.BackgroundTransparency = 0.1
     Desc.BorderSizePixel = 0
-    Desc.Size = UDim2.new(0, 325, 0, 50)
-    Desc.BackgroundColor3 = Color3.FromRGB(40,40,40)
+    Desc.Size = UDim2.new(1, 0, 0, 50)
+    Desc.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
 
     local KeyInput = Instance.new("TextBox", Frame)
     KeyInput.Name = "KeyInput"
-    KeyInput.Font = Enum.Font.BuilderSansExtraBold
+    KeyInput.Font = Enum.Font.SourceSansBold
     KeyInput.FontSize = Enum.FontSize.Size18
     KeyInput.PlaceholderText = "Enter Key"
     KeyInput.Text = ""
     KeyInput.TextColor3 = Color3.fromRGB(255, 255, 255)
-    KeyInput.Size = UDim2.new(0, 305, 0, 40)
+    KeyInput.Size = UDim2.new(1, -20, 0, 40)
     KeyInput.Position = UDim2.new(0, 10, 0, 100)
     KeyInput.BackgroundTransparency = 0.1
     KeyInput.BorderSizePixel = 0
-    KeyInput.BackgroundColor3 = Color3.FromRGB(40,40,40)
+    KeyInput.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
     Instance.new("UICorner", KeyInput)
-    
+
     local GetKey = Instance.new("TextButton", Frame)
     GetKey.Name = "GetKey"
-    GetKey.Font = Enum.Font.BuilderSansExtraBold
+    GetKey.Font = Enum.Font.SourceSansBold
     GetKey.FontSize = Enum.FontSize.Size18
     GetKey.Text = "Get Key"
     GetKey.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -83,7 +83,7 @@ function lib:Create(key, link, script)
 
     local CheckKey = Instance.new("TextButton", Frame)
     CheckKey.Name = "CheckKey"
-    CheckKey.Font = Enum.Font.BuilderSansExtraBold
+    CheckKey.Font = Enum.Font.SourceSansBold
     CheckKey.FontSize = Enum.FontSize.Size18
     CheckKey.Text = "Check Key"
     CheckKey.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -97,11 +97,11 @@ function lib:Create(key, link, script)
         if KeyInput.Text == key then
             print("Key is correct! Loading other script...")
             Key:Destroy()
-            loadstring(script)() 
+            loadstring(script)()
         else
             KeyInput.Text = "The key is invalid! Try again."
             wait(2)
-            KeyInput.Text = "Enter Key"
+            KeyInput.Text = ""
         end
     end
 
